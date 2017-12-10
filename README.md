1bf40ed817bc8af653fd83296570cce71ed3f8c5
石川泰式 <yasunori@ymbp.local>
2017-12-10 22:44:55 +0900

    初版

---
+++ README.md
@@ -0,0 +1,7 @@
+# README
+
+[グレンジ Advent Calendar 2017][advent2017]の記事で利用したプログラムです．
+
+[//]:#(RefUrlStart)
+[advent2017]: https://qiita.com/YasunoriISHIKAWA/private/7abab926c849afcb3a7f
+[//]:#(RefUrlEnd)
---
+++ php-fpm/image/app/index.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>グレンジ Advent Calendar 2017</title>
+
+    <style>
+        body {
+            font-family: Verdana, sans-serif;
+            font-size: 14px;
+            padding: 40px;
+        }
+    </style>
+</head>
+<body>
+    <h1>グレンジ Advent Calendar 2017</h1>
+    <p>Today is <?php echo date('m/d/Y H:i:s'); ?>.</p>
+</body>
+</html>
